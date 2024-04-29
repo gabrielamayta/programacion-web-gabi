907879753b79281ce89828b8e31db7dee3831a0c
gabrielamayta <gabrielamayta2109@gmail.com>
2024-04-29 17:06:42 -0300

    terminar trabajo de pokemon, me falta un pokemon

---
+++ HTML/ejercitacion,html.css/ejercicioJS/script.js
@@ -0,0 +1,29 @@
+// declarar funciones
+
+//funcion tradicional
+
+/*
+function myfunction() {
+    return
+}
+*/
+
+//function flecha o arrow function
+/*
+const myfuction = () =>{
+    
+}*/
+
+let contador = 0;
+// dato importante=> siempre los nombres de las funciones siguen la nomeclatura camelCase
+
+function incrementarNumero() {
+  let number = document.getElementById("numero").textContent;
+  contador = contador + 1;
+  document.getElementById("numero").textContent = contador;
+}
+
+<h1>Calculadora de sumas</h1>
+<label for="number1" >numero1</label> 
+
+}
