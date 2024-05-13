d32dbfb4d8277cbc21c4048b96ce4079d22bfa7d
gabrielamayta <gabrielamayta2109@gmail.com>
2024-05-13 17:08:01 -0300

    ejercicios js listo hasta el 3

---
+++ HTML/ArraysJS/3-iteracion-arrays/ejercicio3.js
@@ -0,0 +1,17 @@
+while (contador < array.length) {
+    console.log(array[contador]);
+    contador++;
+
+}
+
+//for
+
+for(let i =0; 1 < array.length; 1++) {
+    console,log[(1), array(1)];
+
+}
+
+//foreach
+//array.forEach(element)=> {
+//console.log(element);
+//};/*
